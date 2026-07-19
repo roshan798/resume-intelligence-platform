93e6464d68ecebfb107784bc22f0ef33b10f0cf5
Roshan Kumar <rsahu7989@gmail.com>
2026-07-19 13:23:57 +0530

    feat: add global breadcrumbs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Noto_Sans, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Suspense } from "react";
+import { GlobalBreadcrumbs } from "@/components/navigation/global-breadcrumbs";
 
 const playfairDisplayHeading = Playfair_Display({subsets:['latin'],variable:'--font-heading'});
 
@@ -32,7 +34,12 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", notoSans.variable, playfairDisplayHeading.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Suspense fallback={null}>
+          <GlobalBreadcrumbs />
+        </Suspense>
+        {children}
+      </body>
     </html>
   );
 }
